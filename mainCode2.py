b17492922674d2655724dd7460fa49becb5ade2e
nhowe <ngh.howe@gmail.com>
2024-06-06 22:18:29 -0400

    polished data prep

--- mainCode2.py
+++ mainCode2.py
@@ -20,19 +20,49 @@ group_name = 'groupCNNa'
 threshold = 0.7
 trade_window = 12  # distance to predict price delta and trade opportunity
 window_size = 20  # window size (for CNN lookback)
+epochs = 200
+batch_size = 6
+d_epochs = 100
+d_batch_size = 12
 
 # retrieve historical data : [datetime,close,open,high,low,vol,obv,rsi,atr,macd]
 data = csvToArray('historical_data/SPY5min_rawCombinedFiltered.csv')[:-trade_window]
 
+# TODO: make function to create training labels and save them
+
+# drop vals to not use
+np.delete(data, 5, axis=1)  # removes vol
+np.delete(data, 4, axis=1)   # removes low
+np.delete(data, 3, axis=1)   # removes high
+np.delete(data, 2, axis=1)   # removes open
+
 # Transform data to delta values # TODO: pick which indicators to diff
-delta_data = data[['close', 'OBV', 'EMA']].diff().dropna()
+# TODO: get all delta vals then combine w norm vals
+# copy data, drop vals to not diff (datetime, etc), get diff vals, combine delta w/ data
+# delta_data = data[['close', 'OBV', 'EMA']].diff().dropna()
+
+# -------------
+# Compute delta values
+delta_df = data.diff().fillna(0)  # Fill NaN values with 0 for the first row
+
+# Convert delta values back to NumPy array
+delta_data = delta_df.to_numpy()
+
+# Drop delta_data: time; Drop data: close
+np.delete(delta_data, 0, axis=1)
+np.delete(data, 1, axis=1)
+
+# Concatenate original data and delta values along the feature axis
+combined_data = np.concatenate((data, delta_data), axis=1)
+# --------------
 
 # Normalize the data # TODO: pick which features to scale
+# ex. if x has domain [-5,5], scaling [0,1] will turn -2 into a 0.3 (negative vals under 0.5 if even distribution)
 scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(delta_data[['stock_price', 'indicator_1', 'indicator_2']])
+scaled_data = scaler.fit_transform(combined_data)
 
 
-# Create dataset function
+# Create dataset function (preps data for CNN autoencoder)
 def create_dataset(data, window_size):
     X = []
     for i in range(len(data) - window_size):
@@ -109,7 +139,8 @@ split_index = int(len(X) * 0.8)
 X_train, X_test = X[:split_index], X[split_index:]
 y_train, y_test = y[:split_index], y[split_index:]
 num_inputs = X_train.shape[2]  # TODO: confirm this is correct, should equal num of inputs
-print(X_train.shape)
+print(f'X_train shape: {X_train.shape} ...is [2] == num of inputs?')
+print(f'y_train shape: {y_train.shape} ...is [0] == X_train[0]')
 
 # Define the CNN Autoencoder model
 input_layer = Input(shape=(window_size, num_inputs))  # window_size * # of inputs
@@ -132,8 +163,14 @@ autoencoder = Model(input_layer, decoded)
 autoencoder.compile(optimizer='adam', loss='mse')
 
 # Train the autoencoder
-autoencoder.fit(X_train, X_train, epochs=50, batch_size=32, validation_data=(X_test, X_test))
+autoencoder.fit(X_train, X_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, X_test))
 autoencoder.save(f'models/{group_name}/{encoder_name}.keras')
+
+# Print evaluation of the autoencoder
+train_loss = autoencoder.evaluate(X_train, X_train)
+print(f"Training Loss: {train_loss}")
+test_loss = autoencoder.evaluate(X_test, X_test)
+print(f"Test Loss: {test_loss}")
 
 # Create the encoder model
 encoder = Model(input_layer, encoded)
@@ -152,7 +189,8 @@ test_combined = np.hstack((encoded_test_flat, X_test[:, -1, :]))
 
 
 # Define the price delta prediction model
-# samples, window_size / 4, # filters in last CNN] -> 20 / 4 * 64 = 320
+# [samples, window_size / 4, # filters in last CNN]
+#          -flatten & concat-> [samples, (20 / 4 * 64 = 320) + # of features] -> [samples, 320 + # of features]
 delta_model = Sequential([
     Dense(320, activation='relu', input_shape=(train_combined.shape[1],)),
     Dense(160, activation='relu'),
@@ -164,15 +202,15 @@ delta_model = Sequential([
 delta_model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
 # Train the model
-delta_model.fit(train_combined, y_train, epochs=10, batch_size=32, validation_data=(test_combined, y_test))
+delta_model.fit(train_combined, y_train, epochs=d_epochs, batch_size=d_batch_size, validation_data=(test_combined, y_test))
 delta_model.save(f'models/groupCNNa/{delta_model_name}.keras')
 
 display_test_results2(delta_model, test_combined, y_test)
 
 """
 inputs for delta model:
-- relative time of day
-- close price delta 
+- relative time of day (drop delta time of day)
+- close price delta (drop close price)
 - vol (maybe get rid of this)
 - RSI (to indicate overbought/oversold)
 - ?RSI delta 
@@ -186,6 +224,7 @@ inputs for delta model:
 
 - close price delta patterns will be identified with CNN
 - LSTM might be used to... ???
+- LSTM returns output of shape [batch_size, number of units]
 
 output for delta model
 - prediction of max up/down price delta for desired window
